fe2edde9fd708e27f8df3bdb7d6ef5ea770cae11
stevekutz <stkutz@gmail.com>
2020-01-22 22:05:57 -0600

    added simple TabItem component

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import {Tabs, TabItem} from './comp/TabItem';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Tabs defaultIndex="1" onTabClick={console.log}>
+            <TabItem label="A" index="1">
+                Lorem ipsum
+            </TabItem>
+            <TabItem label="B" index="2">
+                Dolor sit amet
+            </TabItem>
+        </Tabs>
     </div>
   );
 }
