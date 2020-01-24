48e354054ad1a07408ff7bc46af12565f8e5b993
stevekutz <stkutz@gmail.com>
2020-01-23 19:36:46 -0600

    added register component

---
+++ app/src/comp/login/login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import loginIMG from '../../loginIMG.png'
+
+export class Login extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div className = "base-container" >
+                <div className = "header">Login</div>
+                <div className = "content">
+                    <div className = "image">
+                        <img src = {loginIMG}/>
+                    </div>
+                    <div className = "form">
+                        <div className = "form-group">
+                            <label htmlFor = "username">Username</label>
+                            <input type = "text" name = "username" placeholder = "username"/>
+                        </div>
+                        <div>
+                            <label htmlFor = "password">Password</label>
+                            <input type = "password" name = "password" placeholder = "password"/>
+                        </div>
+                    </div>               
+                </div>
+                <div className = "footer">
+                    <button type = "button" className = "btn">Login</button>
+                </div>
+            </div>
+        );
+   }
+}
