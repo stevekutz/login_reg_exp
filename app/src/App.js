3dcb3258fc70f3b5cb00dd3b8933ee404741b75b
stevekutz <stkutz@gmail.com>
2020-01-24 22:13:40 -0600

    setting up logic between login & register

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,65 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 // import {Tabs, TabItem} from './comp/tab/TabItem';
-import {Login} from './comp/login/index';
+import {Login, Register} from './comp/login/index';
 
  // node-sass -v    // 4.13.1
 
 
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogginActive: true
+        }; 
+
+    }
+
+    // componentDidMount() {
+    //     this.rightSide.classList.add("right");
+    // }
+
+    // changeState() {
+    //     const {isLogginActive} = this.state;
+
+
+
+    // }
+
+    render() {
+        const {isLogginActive} = this.state;
+                                            //  we set up a reference to the container  div 
+        return (                            // this will verify current reference for transition effects
+            <div className = "App">
+                <div className = "login">  
+                    <div className = "container"></div>
+                        {isLogginActive && <Login containerRef = {(ref) => this.current = ref} />}
+                        {!isLogginActive && <Register containerRef = {(ref) => this.current = ref} />}
+                
+                
+                
+                </div>
+
+            
+            </div>
+        )
+
+
+    }
+
+
+
+}
+
+
+
+
+export default App;
+
+
+
+/* 
 function App() {
     
 
@@ -19,3 +72,5 @@ function App() {
 }
 
 export default App;
+
+*/
